7fa90d79658bc4662e5b6e4ee2add48e21253970
Tashrik Anam <anampartho31@gmail.com>
2021-12-13 17:30:40 +0600

    Add input component

---
+++ src/components/input.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export class Input extends Component {
+  getInputClasses = () => {
+    const { inputClasses } = this.props;
+    let classes = 'form-control';
+    classes += inputClasses ? ' ' + inputClasses : '';
+    return classes;
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor=''>{this.props.title}</label>
+        <input
+          type={this.props.type}
+          className={this.getInputClasses()}
+          placeholder={this.props.placeholder}
+        />
+        {this.props.children}
+      </React.Fragment>
+      // <label for="exampleFormControlInput1" class="form-label">Email address</label>
+      // <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+    );
+  }
+}
+
+export default Input;
