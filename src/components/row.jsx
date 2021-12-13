e036444d8d0749853e81f32578d6c7157eed215d
Tashrik Anam <anampartho31@gmail.com>
2021-12-13 18:22:56 +0600

    Update rows for extraClasses

--- src/components/row.jsx
+++ src/components/row.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
 export class Row extends Component {
+  getExtraClasses = () => {
+    let classes = 'row';
+    const { extraClasses } = this.props;
+    if (this.props.extraClasses) {
+      classes += ' ' + extraClasses;
+    }
+
+    return classes;
+  };
   render() {
-    return <div className='row'>{this.props.children}</div>;
+    return <div className={this.getExtraClasses()}>{this.props.children}</div>;
   }
 }
 
