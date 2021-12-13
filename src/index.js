b5b7d9efcf02802a70053c48028c5aff2a0e1d3d
Tashrik Anam <anampartho31@gmail.com>
2021-12-13 17:28:33 +0600

    Load bootstrap and fontawesome

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
